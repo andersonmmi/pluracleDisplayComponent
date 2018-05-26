8851ff2c1310d047c88db8d88e426b836b2e1979
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-26 17:16:49 -0300

    trims web3 function calls because they were becoming spaghetti code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 //import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
+// import getWeb3 from './utils/getWeb3'
 
 import Ticker from './displayComponent/Ticker.js'
 
@@ -14,107 +14,99 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
-      web3: null,
-      RR: null,
-      web3error: null,
-      account: null,
-      networkId: null,
-      netIdError: null,
-
     }
   }
 
-  componentWillMount() {
-    /** Get network provider and web3 instance.
-     See utils/getWeb3 for more info. */
-    getWeb3
-    .then(results => {
-      // console.log('results: ', results);
-      this.setState({
-        web3: results.web3
-      })
-    })
-    .catch(error => {
-      // console.log(error)
-      this.setState({
-        web3error: error.error
-      })
-    })
-    // this.accountListener()
-  }
-
-  componentDidUpdate(prevProps, prevState){
-  if(prevState.web3!==this.state.web3) {
-    this.whichNetwork();
-  }
-  if(prevState.networkId!==this.state.networkId) {
-    this.reportNetwork(this.state.networkId);
-  }
-  if(this.state.web3!==null) {
-    this.accountListener()
-  }
-}
-
-whichNetwork = () => {
-  this.state.web3.version.getNetwork((err, netId) => {
-    if(err){console.log('err: ', err)}
-    this.setState({networkId: netId});
-  })
-}
-
-reportNetwork = (netId) => {
-  if(netId!=="3"){
-    this.setState({netIdError: "You must be on the Ropsten network!"})
-  }
-  if(netId==="3"){
-    this.setState({netIdError: null})
-  }
-}
-
-accountListener = () => {
-  // var account = web3.eth.accounts[0];
-  setInterval(() => {
-    if (this.state.web3.eth.accounts[0] !== this.state.account) {
-      console.log('this.state.web3.eth.accounts[0]: ', this.state.web3.eth.accounts[0]);
-      console.log('this.state.account: ', this.state.account)
-      // account = web3.eth.accounts[0];
-      // updateInterface();
-      this.setState({account: this.state.web3.eth.accounts[0]})
-    }
-  }, 2000);
-}
-  // instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-    // const contract = require('truffle-contract')
-    // const simpleStorage = contract(SimpleStorageContract)
-    // simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    // var simpleStorageInstance
-
-    // Get accounts.
-  //   this.state.web3.eth.getAccounts((error, accounts) => {
-  //     simpleStorage.deployed().then((instance) => {
-  //       simpleStorageInstance = instance
-  //
-  //       // Stores a given value, 5 by default.
-  //       return simpleStorageInstance.set(5, {from: accounts[0]})
-  //     }).then((result) => {
-  //       // Get the value from the contract to prove it worked.
-  //       return simpleStorageInstance.get.call(accounts[0])
-  //     }).then((result) => {
-  //       // Update state with the result.
-  //       return this.setState({ storageValue: result.c[0] })
-  //     })
-  //   })
-  // }
+//   componentWillMount() {
+//     /** Get network provider and web3 instance.
+//      See utils/getWeb3 for more info. */
+//     getWeb3
+//     .then(results => {
+//       // console.log('results: ', results);
+//       this.setState({
+//         web3: results.web3
+//       })
+//     })
+//     .catch(error => {
+//       // console.log(error)
+//       this.setState({
+//         web3error: error.error
+//       })
+//     })
+//     // this.accountListener()
+//   }
+//
+//   componentDidUpdate(prevProps, prevState){
+//   if(prevState.web3!==this.state.web3) {
+//     this.whichNetwork();
+//   }
+//   if(prevState.networkId!==this.state.networkId) {
+//     this.reportNetwork(this.state.networkId);
+//   }
+//   if(this.state.web3!==null) {
+//     this.accountListener()
+//   }
+// }
+//
+// whichNetwork = () => {
+//   this.state.web3.version.getNetwork((err, netId) => {
+//     if(err){console.log('err: ', err)}
+//     this.setState({networkId: netId});
+//   })
+// }
+//
+// reportNetwork = (netId) => {
+//   if(netId!=="3"){
+//     this.setState({netIdError: "You must be on the Ropsten network!"})
+//   }
+//   if(netId==="3"){
+//     this.setState({netIdError: null})
+//   }
+// }
+//
+// accountListener = () => {
+//   // var account = web3.eth.accounts[0];
+//   setInterval(() => {
+//     if (this.state.web3.eth.accounts[0] !== this.state.account) {
+//       console.log('this.state.web3.eth.accounts[0]: ', this.state.web3.eth.accounts[0]);
+//       console.log('this.state.account: ', this.state.account)
+//       // account = web3.eth.accounts[0];
+//       // updateInterface();
+//       this.setState({account: this.state.web3.eth.accounts[0]})
+//     }
+//   }, 2000);
+// }
+//   // instantiateContract() {
+//     /*
+//      * SMART CONTRACT EXAMPLE
+//      *
+//      * Normally these functions would be called in the context of a
+//      * state management library, but for convenience I've placed them here.
+//      */
+//
+//     // const contract = require('truffle-contract')
+//     // const simpleStorage = contract(SimpleStorageContract)
+//     // simpleStorage.setProvider(this.state.web3.currentProvider)
+//
+//     // Declaring this for later so we can chain functions on SimpleStorage.
+//     // var simpleStorageInstance
+//
+//     // Get accounts.
+//   //   this.state.web3.eth.getAccounts((error, accounts) => {
+//   //     simpleStorage.deployed().then((instance) => {
+//   //       simpleStorageInstance = instance
+//   //
+//   //       // Stores a given value, 5 by default.
+//   //       return simpleStorageInstance.set(5, {from: accounts[0]})
+//   //     }).then((result) => {
+//   //       // Get the value from the contract to prove it worked.
+//   //       return simpleStorageInstance.get.call(accounts[0])
+//   //     }).then((result) => {
+//   //       // Update state with the result.
+//   //       return this.setState({ storageValue: result.c[0] })
+//   //     })
+//   //   })
+//   // }
 
   render() {
     return (
--- src/displayComponent/Ticker.js
+++ src/displayComponent/Ticker.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 //import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from '../utils/getWeb3'
-import Web3 from 'web3';
+// import Web3 from 'web3';
 import _ from 'lodash';
 
 import '../css/oswald.css'
@@ -9,126 +9,8 @@ import '../css/open-sans.css'
 import '../css/pure-min.css'
 import './Ticker.css'
 
-let web3
-
-let abi = [
- {
-  "anonymous": false,
-  "inputs": [
-   {
-    "indexed": true,
-    "name": "previousOwner",
-    "type": "address"
-   },
-   {
-    "indexed": true,
-    "name": "newOwner",
-    "type": "address"
-   }
-  ],
-  "name": "OwnershipTransferred",
-  "type": "event"
- },
- {
-  "constant": false,
-  "inputs": [
-   {
-    "name": "newOwner",
-    "type": "address"
-   }
-  ],
-  "name": "transferOwnership",
-  "outputs": [],
-  "payable": false,
-  "stateMutability": "nonpayable",
-  "type": "function"
- },
- {
-  "constant": false,
-  "inputs": [
-   {
-    "name": "data",
-    "type": "bytes32"
-   }
-  ],
-  "name": "update",
-  "outputs": [],
-  "payable": false,
-  "stateMutability": "nonpayable",
-  "type": "function"
- },
- {
-  "inputs": [
-   {
-    "name": "dataType",
-    "type": "string"
-   }
-  ],
-  "payable": false,
-  "stateMutability": "nonpayable",
-  "type": "constructor"
- },
- {
-  "constant": true,
-  "inputs": [],
-  "name": "data",
-  "outputs": [
-   {
-    "name": "",
-    "type": "bytes32"
-   }
-  ],
-  "payable": false,
-  "stateMutability": "view",
-  "type": "function"
- },
- {
-  "constant": true,
-  "inputs": [],
-  "name": "dataType",
-  "outputs": [
-   {
-    "name": "",
-    "type": "string"
-   }
-  ],
-  "payable": false,
-  "stateMutability": "view",
-  "type": "function"
- },
- {
-  "constant": true,
-  "inputs": [],
-  "name": "lastTimestamp",
-  "outputs": [
-   {
-    "name": "",
-    "type": "uint256"
-   }
-  ],
-  "payable": false,
-  "stateMutability": "view",
-  "type": "function"
- },
- {
-  "constant": true,
-  "inputs": [],
-  "name": "owner",
-  "outputs": [
-   {
-    "name": "",
-    "type": "address"
-   }
-  ],
-  "payable": false,
-  "stateMutability": "view",
-  "type": "function"
- }
-];
+let web3, oracleContract, oc
 let address = '0xd64f463affca20ea99059ba42ed13c979587a117';
-// let oracleContract = web3.eth.contract(abi).at(address);
-// web3.eth.defaultAccount = web3.eth.accounts[0];
-
 
 
 class Ticker extends Component {
@@ -136,109 +18,20 @@ class Ticker extends Component {
     super(props)
 
     this.state = {
-      firstNames: [],
-      lastNames: [],
-      ages: [],
       web3: null,
+      data: null,
     }
   }
 
-  componentWillMount() {
-    /** Get network provider and web3 instance.
-     See utils/getWeb3 for more info. */
-    getWeb3
-    .then(results => {
-      // console.log('results: ', results);
-      web3 = results.web3
-      this.setState({
-        web3: results.web3
-      })
-    })
-    .catch(error => {
-      // console.log(error)
-      this.setState({
-        web3error: error.error
-      })
-    })
-    // this.accountListener()
-    // var data = peopleContract.getPeople();
-    // this.setState({
-    //   firstNames: String(data[0]).split(','),
-    //   lastNames: String(data[1]).split(','),
-    //   ages: String(data[2]).split(',')
-    // });
-    // console.log(Date.now());
-    console.log(this.state.web3);
-  }
-
-  componentDidUpdate(prevProps, prevState){
-  if(prevState.web3!==this.state.web3) {
-    this.whichNetwork();
-  }
-  if(prevState.networkId!==this.state.networkId) {
-    this.reportNetwork(this.state.networkId);
-  }
-  if(this.state.web3!==null) {
-    this.accountListener()
-  }
-}
-
-whichNetwork = () => {
-  this.state.web3.version.getNetwork((err, netId) => {
-    if(err){console.log('err: ', err)}
-    this.setState({networkId: netId});
-  })
-}
-
-reportNetwork = (netId) => {
-  if(netId!=="3"){
-    this.setState({netIdError: "You must be on the Ropsten network!"})
-  }
-  if(netId==="3"){
-    this.setState({netIdError: null})
-  }
-}
-
-accountListener = () => {
-  // var account = web3.eth.accounts[0];
-  setInterval(() => {
-    if (this.state.web3.eth.accounts[0] !== this.state.account) {
-      console.log('this.state.web3.eth.accounts[0]: ', this.state.web3.eth.accounts[0]);
-      console.log('this.state.account: ', this.state.account)
-      console.log('this.state.web3:', this.state.web3);
-      // account = web3.eth.accounts[0];
-      // updateInterface();
-      this.setState({account: this.state.web3.eth.accounts[0]})
-    }
-  }, 2000);
-}
-
-
-  componentDidMount() {
-    // setInterval(()=>{
-    //     var data = peopleContract.getPeople();
-    //     this.setState({
-    //       firstNames: String(data[0]).split(','),
-    //       lastNames: String(data[1]).split(','),
-    //       ages: String(data[2]).split(',')
-    //     });
-    //     // console.log(Date.now());
-    //
-    //   },
-    //   30000
-    // )
-  }
-
-
-
+  
   render() {
 
     let data = []
 
-    _.each(this.state.firstNames, (value, index) => {
+    _.each(this.state.data, (value, index) => {
       data.push(
         <div className="ticker__item" key={index}>
-          {web3.toAscii(this.state.firstNames[index])} {web3.toAscii(this.state.lastNames[index])}: {this.state.ages[index]}
+          {this.state.data}
         </div>
       )
     })
