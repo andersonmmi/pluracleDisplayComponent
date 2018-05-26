1602ff233fbac977d002ce10c4d8bba9dbe45311
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-26 11:56:14 -0300

    ticker pulls data from contract and displays it

--- src/displayComponent/Ticker.js
+++ src/displayComponent/Ticker.js
@@ -1,25 +1,118 @@
 import React, { Component } from 'react'
 //import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
 import getWeb3 from '../utils/getWeb3'
+import Web3 from 'web3';
+import _ from 'lodash';
 
 import '../css/oswald.css'
 import '../css/open-sans.css'
 import '../css/pure-min.css'
 import './Ticker.css'
 
+let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
+let peopleContractABI = [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "getPeople",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bytes32[]"
+        },
+        {
+          "name": "",
+          "type": "bytes32[]"
+        },
+        {
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "payable": false,
+      "type": "function",
+      "stateMutability": "view"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_firstName",
+          "type": "bytes32"
+        },
+        {
+          "name": "_lastName",
+          "type": "bytes32"
+        },
+        {
+          "name": "_age",
+          "type": "uint256"
+        }
+      ],
+      "name": "addPerson",
+      "outputs": [
+        {
+          "name": "success",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "type": "function",
+      "stateMutability": "nonpayable"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "people",
+      "outputs": [
+        {
+          "name": "firstName",
+          "type": "bytes32"
+        },
+        {
+          "name": "lastName",
+          "type": "bytes32"
+        },
+        {
+          "name": "age",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "type": "function",
+      "stateMutability": "view"
+    }
+  ];
+let peopleContractAddress = '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf';
+let peopleContract = web3.eth.contract(peopleContractABI).at(peopleContractAddress);
+web3.eth.defaultAccount = web3.eth.accounts[0];
+let balance = web3.fromWei(web3.eth.getBalance(web3.eth.coinbase)).toString();
+
 class Ticker extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      data: '',
-      web3: null
+      firstNames: [],
+      lastNames: [],
+      ages: [],
     }
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
+    var data = peopleContract.getPeople();
+    this.setState({
+      firstNames: String(data[0]).split(','),
+      lastNames: String(data[1]).split(','),
+      ages: String(data[2]).split(',')
+    });
+    console.log(typeof data);
+    console.log(data);
 
     getWeb3
     .then(results => {
@@ -35,48 +128,25 @@ class Ticker extends Component {
     })
   }
 
-  // instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
 
-    // const contract = require('truffle-contract')
-    // const simpleStorage = contract(SimpleStorageContract)
-    // simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    // var simpleStorageInstance
-
-    // Get accounts.
-  //   this.state.web3.eth.getAccounts((error, accounts) => {
-  //     simpleStorage.deployed().then((instance) => {
-  //       simpleStorageInstance = instance
-  //
-  //       // Stores a given value, 5 by default.
-  //       return simpleStorageInstance.set(5, {from: accounts[0]})
-  //     }).then((result) => {
-  //       // Get the value from the contract to prove it worked.
-  //       return simpleStorageInstance.get.call(accounts[0])
-  //     }).then((result) => {
-  //       // Update state with the result.
-  //       return this.setState({ storageValue: result.c[0] })
-  //     })
-  //   })
-  // }
 
   render() {
+
+    let data = []
+
+    _.each(this.state.firstNames, (value, index) => {
+      data.push(
+        <div className="ticker__item" key={index}>
+          {web3.toAscii(this.state.firstNames[index])} {web3.toAscii(this.state.lastNames[index])}: {this.state.ages[index]}
+        </div>
+      )
+    })
+
     return (
       <div className="displayComponent">
         <div className="ticker-wrap">
           <div className="ticker">
-            <div className="ticker__item">BTC: $10,000</div>
-            <div className="ticker__item">ETH: $1,000</div>
-            <div className="ticker__item">TRTL: $0.10</div>
-            <div className="ticker__item">Pluracle: Awesome!</div>
-            <div className="ticker__item">More Data: More Data</div>
+            {data}
           </div>
         </div>
       </div>
