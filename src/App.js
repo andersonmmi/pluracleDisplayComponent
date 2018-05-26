da5639971d2f5ee62d14e859e4ea1ce9da39171c
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-26 14:56:55 -0300

    app renders if web3 is detected

--- src/App.js
+++ src/App.js
@@ -15,28 +15,75 @@ class App extends Component {
 
     this.state = {
       storageValue: 0,
-      web3: null
+      web3: null,
+      RR: null,
+      web3error: null,
+      account: null,
+      networkId: null,
+      netIdError: null,
+
     }
   }
 
   componentWillMount() {
-    // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
-
+    /** Get network provider and web3 instance.
+     See utils/getWeb3 for more info. */
     getWeb3
     .then(results => {
+      // console.log('results: ', results);
       this.setState({
         web3: results.web3
       })
-
-      // Instantiate contract once web3 provided.
-      // this.instantiateContract()
     })
-    .catch(() => {
-      console.log('Error finding web3.')
+    .catch(error => {
+      // console.log(error)
+      this.setState({
+        web3error: error.error
+      })
     })
+    // this.accountListener()
   }
 
+  componentDidUpdate(prevProps, prevState){
+  if(prevState.web3!==this.state.web3) {
+    this.whichNetwork();
+  }
+  if(prevState.networkId!==this.state.networkId) {
+    this.reportNetwork(this.state.networkId);
+  }
+  if(this.state.web3!==null) {
+    this.accountListener()
+  }
+}
+
+whichNetwork = () => {
+  this.state.web3.version.getNetwork((err, netId) => {
+    if(err){console.log('err: ', err)}
+    this.setState({networkId: netId});
+  })
+}
+
+reportNetwork = (netId) => {
+  if(netId!=="3"){
+    this.setState({netIdError: "You must be on the Ropsten network!"})
+  }
+  if(netId==="3"){
+    this.setState({netIdError: null})
+  }
+}
+
+accountListener = () => {
+  // var account = web3.eth.accounts[0];
+  setInterval(() => {
+    if (this.state.web3.eth.accounts[0] !== this.state.account) {
+      console.log('this.state.web3.eth.accounts[0]: ', this.state.web3.eth.accounts[0]);
+      console.log('this.state.account: ', this.state.account)
+      // account = web3.eth.accounts[0];
+      // updateInterface();
+      this.setState({account: this.state.web3.eth.accounts[0]})
+    }
+  }, 2000);
+}
   // instantiateContract() {
     /*
      * SMART CONTRACT EXAMPLE
@@ -88,7 +135,9 @@ class App extends Component {
             </div>
           </div>
         </main>
+
         <Ticker />
+
       </div>
     );
   }
--- src/displayComponent/Ticker.js
+++ src/displayComponent/Ticker.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 //import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-// import getWeb3 from '../utils/getWeb3'
+import getWeb3 from '../utils/getWeb3'
 import Web3 from 'web3';
 import _ from 'lodash';
 
@@ -9,88 +9,127 @@ import '../css/open-sans.css'
 import '../css/pure-min.css'
 import './Ticker.css'
 
-let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"))
-let peopleContractABI = [
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getPeople",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bytes32[]"
-        },
-        {
-          "name": "",
-          "type": "bytes32[]"
-        },
-        {
-          "name": "",
-          "type": "uint256[]"
-        }
-      ],
-      "payable": false,
-      "type": "function",
-      "stateMutability": "view"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_firstName",
-          "type": "bytes32"
-        },
-        {
-          "name": "_lastName",
-          "type": "bytes32"
-        },
-        {
-          "name": "_age",
-          "type": "uint256"
-        }
-      ],
-      "name": "addPerson",
-      "outputs": [
-        {
-          "name": "success",
-          "type": "bool"
-        }
-      ],
-      "payable": false,
-      "type": "function",
-      "stateMutability": "nonpayable"
-    },
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "people",
-      "outputs": [
-        {
-          "name": "firstName",
-          "type": "bytes32"
-        },
-        {
-          "name": "lastName",
-          "type": "bytes32"
-        },
-        {
-          "name": "age",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "type": "function",
-      "stateMutability": "view"
-    }
-  ];
-let peopleContractAddress = '0xf25186b5081ff5ce73482ad761db0eb0d25abfbf';
-let peopleContract = web3.eth.contract(peopleContractABI).at(peopleContractAddress);
-web3.eth.defaultAccount = web3.eth.accounts[0];
+let web3
+//
+// let ABI = [
+//  {
+//   "anonymous": false,
+//   "inputs": [
+//    {
+//     "indexed": true,
+//     "name": "previousOwner",
+//     "type": "address"
+//    },
+//    {
+//     "indexed": true,
+//     "name": "newOwner",
+//     "type": "address"
+//    }
+//   ],
+//   "name": "OwnershipTransferred",
+//   "type": "event"
+//  },
+//  {
+//   "constant": false,
+//   "inputs": [
+//    {
+//     "name": "newOwner",
+//     "type": "address"
+//    }
+//   ],
+//   "name": "transferOwnership",
+//   "outputs": [],
+//   "payable": false,
+//   "stateMutability": "nonpayable",
+//   "type": "function"
+//  },
+//  {
+//   "constant": false,
+//   "inputs": [
+//    {
+//     "name": "data",
+//     "type": "bytes32"
+//    }
+//   ],
+//   "name": "update",
+//   "outputs": [],
+//   "payable": false,
+//   "stateMutability": "nonpayable",
+//   "type": "function"
+//  },
+//  {
+//   "inputs": [
+//    {
+//     "name": "dataType",
+//     "type": "string"
+//    }
+//   ],
+//   "payable": false,
+//   "stateMutability": "nonpayable",
+//   "type": "constructor"
+//  },
+//  {
+//   "constant": true,
+//   "inputs": [],
+//   "name": "data",
+//   "outputs": [
+//    {
+//     "name": "",
+//     "type": "bytes32"
+//    }
+//   ],
+//   "payable": false,
+//   "stateMutability": "view",
+//   "type": "function"
+//  },
+//  {
+//   "constant": true,
+//   "inputs": [],
+//   "name": "dataType",
+//   "outputs": [
+//    {
+//     "name": "",
+//     "type": "string"
+//    }
+//   ],
+//   "payable": false,
+//   "stateMutability": "view",
+//   "type": "function"
+//  },
+//  {
+//   "constant": true,
+//   "inputs": [],
+//   "name": "lastTimestamp",
+//   "outputs": [
+//    {
+//     "name": "",
+//     "type": "uint256"
+//    }
+//   ],
+//   "payable": false,
+//   "stateMutability": "view",
+//   "type": "function"
+//  },
+//  {
+//   "constant": true,
+//   "inputs": [],
+//   "name": "owner",
+//   "outputs": [
+//    {
+//     "name": "",
+//     "type": "address"
+//    }
+//   ],
+//   "payable": false,
+//   "stateMutability": "view",
+//   "type": "function"
+//  }
+// ];
+// let Address = '0xd64f463affca20ea99059ba42ed13c979587a117';
+// let peopleContract = web3.eth.contract(ABI).at(Address);
+// web3.eth.defaultAccount = web3.eth.accounts[0];
+
+
 
 class Ticker extends Component {
   constructor(props) {
@@ -100,31 +139,91 @@ class Ticker extends Component {
       firstNames: [],
       lastNames: [],
       ages: [],
+      web3: null,
     }
   }
 
   componentWillMount() {
-    var data = peopleContract.getPeople();
-    this.setState({
-      firstNames: String(data[0]).split(','),
-      lastNames: String(data[1]).split(','),
-      ages: String(data[2]).split(',')
-    });
-    console.log(Date.now());
+    /** Get network provider and web3 instance.
+     See utils/getWeb3 for more info. */
+    getWeb3
+    .then(results => {
+      // console.log('results: ', results);
+      this.setState({
+        web3: results.web3
+      })
+    })
+    .catch(error => {
+      // console.log(error)
+      this.setState({
+        web3error: error.error
+      })
+    })
+    // this.accountListener()
+    // var data = peopleContract.getPeople();
+    // this.setState({
+    //   firstNames: String(data[0]).split(','),
+    //   lastNames: String(data[1]).split(','),
+    //   ages: String(data[2]).split(',')
+    // });
+    // console.log(Date.now());
   }
 
+  componentDidUpdate(prevProps, prevState){
+  if(prevState.web3!==this.state.web3) {
+    this.whichNetwork();
+  }
+  if(prevState.networkId!==this.state.networkId) {
+    this.reportNetwork(this.state.networkId);
+  }
+  if(this.state.web3!==null) {
+    this.accountListener()
+  }
+}
+
+whichNetwork = () => {
+  this.state.web3.version.getNetwork((err, netId) => {
+    if(err){console.log('err: ', err)}
+    this.setState({networkId: netId});
+  })
+}
+
+reportNetwork = (netId) => {
+  if(netId!=="3"){
+    this.setState({netIdError: "You must be on the Ropsten network!"})
+  }
+  if(netId==="3"){
+    this.setState({netIdError: null})
+  }
+}
+
+accountListener = () => {
+  // var account = web3.eth.accounts[0];
+  setInterval(() => {
+    if (this.state.web3.eth.accounts[0] !== this.state.account) {
+      console.log('this.state.web3.eth.accounts[0]: ', this.state.web3.eth.accounts[0]);
+      console.log('this.state.account: ', this.state.account)
+      // account = web3.eth.accounts[0];
+      // updateInterface();
+      this.setState({account: this.state.web3.eth.accounts[0]})
+    }
+  }, 2000);
+}
+
+
   componentDidMount() {
-    setInterval(()=>{
-        var data = peopleContract.getPeople();
-        this.setState({
-          firstNames: String(data[0]).split(','),
-          lastNames: String(data[1]).split(','),
-          ages: String(data[2]).split(',')
-        });
-        console.log(Date.now());
-      },
-      30000
-    )
+    // setInterval(()=>{
+    //     var data = peopleContract.getPeople();
+    //     this.setState({
+    //       firstNames: String(data[0]).split(','),
+    //       lastNames: String(data[1]).split(','),
+    //       ages: String(data[2]).split(',')
+    //     });
+    //     // console.log(Date.now());
+    //
+    //   },
+    //   30000
+    // )
   }
 
 
