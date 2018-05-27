8e7ecd06a5e70f4ea267df73c5ccce03fa55089a
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-26 22:09:41 -0300

    styles App

--- src/App.js
+++ src/App.js
@@ -84,37 +84,6 @@ accountListener = () => {
     }
   }, 2000);
 }
-  // instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
-
-    // const contract = require('truffle-contract')
-    // const simpleStorage = contract(SimpleStorageContract)
-    // simpleStorage.setProvider(this.state.web3.currentProvider)
-
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    // var simpleStorageInstance
-
-    // Get accounts.
-  //   this.state.web3.eth.getAccounts((error, accounts) => {
-  //     simpleStorage.deployed().then((instance) => {
-  //       simpleStorageInstance = instance
-  //
-  //       // Stores a given value, 5 by default.
-  //       return simpleStorageInstance.set(5, {from: accounts[0]})
-  //     }).then((result) => {
-  //       // Get the value from the contract to prove it worked.
-  //       return simpleStorageInstance.get.call(accounts[0])
-  //     }).then((result) => {
-  //       // Update state with the result.
-  //       return this.setState({ storageValue: result.c[0] })
-  //     })
-  //   })
-  // }
 
   render() {
     return (
