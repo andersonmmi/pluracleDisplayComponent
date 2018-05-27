21f600b08a3051b7848f5375b58e9dc2f1ea3957
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-27 00:36:54 -0300

    comment out SourceSelector component

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ accountListener = () => {
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
               <p>The stored value is: {this.state.storageValue}</p>
-              <SourceSelector />
+              {/*<SourceSelector />*/}
 
             </div>
           </div>
--- src/SourceSelector/SourceSelector.js
+++ src/SourceSelector/SourceSelector.js
@@ -4,55 +4,57 @@ import './SourceSelector.css'
 class SourceSelector extends Component {
   constructor(props) {
   super(props);
-  this.state = { isChecked: false };
+  this.state = {
+    account1: false,
+    account2: false,
+    account3: false,
+  };
+  this._handleClick=this._handleClick.bind(this);
 }
 
-
-
-    render() {
-      return (
-        <div>
-          <Checkbox
-            id="checkbox3"
-            value="random3"
-            name="example"
-            label={
-              <span>some unchecked component with event handled by react</span>
-            }
-            isChecked={this.state.isChecked}
-            onChange={() => this._handleClick()}/>
-          <button onClick={() => this._handleClick()}>click</button>
-        </div>
-      );
+    _handleClick = (event) => {
+      event.preventDefault();
+      console.log(event);
+      // this.setState({ [event.target.id]: !this.state.[event.target.id] });
     }
 
-    _handleClick() {
-      this.setState({ isChecked: !this.state.isChecked });
-    }
-  }
-
-  class Checkbox extends React.Component {
     render() {
       return (
         <div>
           <input
             type="checkbox"
-            id={this.props.id}
-            value={this.props.value}
-            name={this.props.name}
-            checked={this.props.isChecked}
-            disabled={this.props.disabled}
-            onChange={ () => this.props.onChange() }/>
-          <label
-            aria-hidden="true"
-            htmlFor={this.props.id}/>
-          <label htmlFor={this.props.id}>
-            {this.props.label}
-          </label>
+            id="account1"
+            name="account1"
+            checked={this.state.account1}
+            onChange={() => this._handleClick()}/>
+            <span>account1</span>
+          <input
+            type="checkbox"
+            id="account2"
+            name="account2"
+            checked={this.state.account2}
+            onChange={() => this._handleClick()}/>
+            <span>account2</span>
+          <input
+            type="checkbox"
+            id="account3"
+            name="account3"
+            checked={this.state.account3}
+            onChange={() => this._handleClick()}/>
+            <span>account3</span>
+
+          <button onClick={() => this._handleClick()}>filter</button>
         </div>
       );
     }
+
+    _handleClick = (event) => {
+      console.log(event);
+      // this.setState({ [event.target.id]: !this.state.[event.target.id] });
+    }
   }
+
+
 
 
 
