662401005eabc48392b819c4e3326d630c8521a8
Aaron Anderson <aaronlynanderson@gmail.com>
2018-05-27 02:46:08 -0300

    generates clickable list of oracles

--- src/SourceSelector/SourceSelector.js
+++ src/SourceSelector/SourceSelector.js
@@ -1,5 +1,328 @@
 import React, { Component } from 'react';
+import getWeb3 from '../utils/getWeb3';
+import Web3 from 'web3';
+import _ from 'lodash';
 import './SourceSelector.css'
+
+let web3
+
+let abi = [
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "name": "roleName",
+          "type": "string"
+        }
+      ],
+      "name": "checkRole",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "name": "roleName",
+          "type": "string"
+        }
+      ],
+      "name": "hasRole",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "name": "roleName",
+          "type": "string"
+        }
+      ],
+      "name": "adminRemoveRole",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "name": "roleName",
+          "type": "string"
+        }
+      ],
+      "name": "adminAddRole",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "ROLE_ADMIN",
+      "outputs": [
+        {
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "oracleType",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "dataType",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "name": "description",
+          "type": "string"
+        }
+      ],
+      "name": "OracleAdded",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "oracleType",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "name": "addr",
+          "type": "address"
+        }
+      ],
+      "name": "OracleRemoved",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "oracleType",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "description",
+          "type": "string"
+        }
+      ],
+      "name": "OracleDescriptionUpdated",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "roleName",
+          "type": "string"
+        }
+      ],
+      "name": "RoleAdded",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "name": "addr",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "name": "roleName",
+          "type": "string"
+        }
+      ],
+      "name": "RoleRemoved",
+      "type": "event"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_type",
+          "type": "string"
+        },
+        {
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "name": "_owner",
+          "type": "address"
+        },
+        {
+          "name": "_dataType",
+          "type": "string"
+        },
+        {
+          "name": "_description",
+          "type": "string"
+        }
+      ],
+      "name": "addOracle",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_address",
+          "type": "address"
+        }
+      ],
+      "name": "removeOracle",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [
+        {
+          "name": "_address",
+          "type": "address"
+        },
+        {
+          "name": "_description",
+          "type": "string"
+        }
+      ],
+      "name": "updateOracleDescription",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "_type",
+          "type": "string"
+        }
+      ],
+      "name": "getOracleList",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address[]"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "addr",
+          "type": "address"
+        }
+      ],
+      "name": "getOracleInfo",
+      "outputs": [
+        {
+          "name": "",
+          "type": "address"
+        },
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "string"
+        },
+        {
+          "name": "",
+          "type": "uint256"
+        },
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+let address = '0x19400099be04c9c720d742784bd5f5e841bce78a';
+
+
 
 class SourceSelector extends Component {
   constructor(props) {
@@ -10,6 +333,56 @@ class SourceSelector extends Component {
     account3: false,
   };
   this.handleClick=this.handleClick.bind(this);
+  this.setOracleList=this.setOracleList.bind(this);
+}
+
+componentWillMount() {
+  /** Get network provider and web3 instance.
+   See utils/getWeb3 for more info. */
+  getWeb3
+  .then(results => {
+    // console.log('results: ', results);
+    web3 = results.web3;
+    let array = []
+    let infoArray = []
+// @dev building the api call
+    let oracleContract = web3.eth.contract(abi).at(address);
+    console.log(oracleContract);
+    oracleContract.getOracleList("signed:uint256", (error, result) => {
+      if(!error){
+          console.log(JSON.stringify(result));
+          array.push(result);
+          this.setOracleList(array);
+      }else{
+          console.error(error);
+      }
+    });
+    this.setState({
+      web3: results.web3,
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    this.setState({
+      web3error: error.error,
+      data: "catch"
+    })
+  })
+  // this.accountListener()
+  // var data = peopleContract.getPeople();
+  // this.setState({
+  //   firstNames: String(data[0]).split(','),
+  //   lastNames: String(data[1]).split(','),
+  //   ages: String(data[2]).split(',')
+  // });
+  // console.log(Date.now());
+  console.log(this.state.web3);
+}
+
+setOracleList = (data) => {
+  this.setState({
+    oracleList: data
+  })
 }
 
   handleClick = (event) => {
@@ -24,44 +397,32 @@ class SourceSelector extends Component {
   }
 
     render() {
+
+        let data = []
+
+        _.each(this.state.oracleList, (value, index) => {
+          data.push(
+             <span className="oracleCheckbox" key={index}>
+             <input
+                type="checkbox"
+                id="account1"
+                name="account1"
+                checked={this.state.account1}
+                onChange={this.handleClick.bind(this)}/>
+                account1
+            </span>
+
+          )
+        })
+
       return (
         <div className="listOfOracles">
-          <span>
-          <input
-            type="checkbox"
-            id="account1"
-            name="account1"
-            checked={this.state.account1}
-            onChange={this.handleClick.bind(this)}/>
-            account1</span>
-          <span>
-          <input
-            type="checkbox"
-            id="account2"
-            name="account2"
-            checked={this.state.account2}
-            onChange={this.handleClick.bind(this)}/>
-            account2</span>
-          <span>
-          <input
-            type="checkbox"
-            id="account3"
-            name="account3"
-            checked={this.state.account3}
-            onChange={this.handleClick.bind(this)}/>
-            account3</span>
+            {data}
 
           <button>filter</button>
         </div>
       );
     }
-
-
   }
-
-
-
-
-
 
 export default SourceSelector;
